fe2ff9894bab3c1bcbf4009dadcc46b555190ddf
Cole Rau <colevrau@gmail.com>
2019-10-06 19:54:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -136,5 +136,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  return programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 end
