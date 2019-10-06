2a34b97ed2820597fb47a0440fa530b5b38b3d3d
Cole Rau <colevrau@gmail.com>
2019-10-06 19:31:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,10 @@ def adding_matz
         }
      }
 
-  programmer_hash = 
+  programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+   }
 
   return programmer_hash
 end
